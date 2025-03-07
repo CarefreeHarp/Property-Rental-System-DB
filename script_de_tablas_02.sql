25074ecf3b53b2bc7b7c4b7be772c773d7316f5a
guille-011 <apontecardenas54321@gmail.com>
2025-03-07 09:19:50 -0500

    tablas completas

--- script_de_tablas_02.sql
+++ script_de_tablas_02.sql
@@ -12,14 +12,31 @@ CREATE TABLE DEUDOR (
 CREATE TABLE PRESTAMO(
     id NUMBER(10,0) GENERATED ALWAYS AS IDENTITY,
     iddeudor NUMBER(10,0) NOT NULL,
---  idbanco NUMBER(10,0) NOT NULL,                                      DESCOMENTAR CUANDO LA TABLA BANCO HAYA SIDO CREADA
+    idbanco NUMBER(10,0) NOT NULL,                                     
     fecha DATE DEFAULT SYSDATE NOT NULL,
     valor_otorgado NUMBER(11,4) DEFAULT 0 NOT NULL CHECK(valor_otorgado >= 0),
     pagado CHAR(2) DEFAULT 'NO' NOT NULL CHECK (pagado IN ('SI','NO')),
 
     PRIMARY KEY (id),
     FOREIGN KEY (iddeudor) REFERENCES DEUDOR(id),
---  FOREIGN KEY (idbanco) REFERENCES BANCO(id),                         DESCOMENTAR CUANDO LA TABLA BANCO HAYA SIDO CREADA
+    FOREIGN KEY (idbanco) REFERENCES BANCO(id),                       
 
-    CONSTRAINT llaves_naturales_prestamo UNIQUE (iddeudor,/* idbanco ,*/ fecha)  --DESCOMENTAR CUANDO LA TABLA BANCO HAYA SIDO CREADA
+    CONSTRAINT llaves_naturales_prestamo UNIQUE (iddeudor,idbanco , fecha)  
+);
+
+CREATE TABLE ABONO(
+    id NUMBER(10,0) GENERATED ALWAYS AS IDENTITY,
+    idprestamo NUMBER(10,0) NOT NULL,
+    fecha DATE DEFAULT SYSDATE NOT NULL,
+    valor_abono NUMBER(11,4) NOT NULL CHECK(valor_abono >= 0),
+    PRIMARY KEY(id),
+    FOREIGN KEY(idprestamo) REFERENCES PRESTAMO(id),
+    CONSTRAINT llaves_naturales_abono UNIQUE(idprestamo, fecha)
+);
+
+CREATE TABLE BANCO(
+    id NUMBER(10,0) GENERATED ALWAYS AS IDENTITY,
+    nombre VARCHAR2(20) NOT NULL,
+    PRIMARY KEY (id),
+    CONSTRAINT llave_natural_banco UNIQUE(nombre)
 );
